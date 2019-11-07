b3e99eab94e04c9e097818111fb8e6d4828fa902
Maddy <madhesh9024.n@gmail.com>
2019-11-07 09:20:26 +0530

    Added some more programs

---
+++ Basics/src/patterns/NumberPattern2.java
@@ -0,0 +1,11 @@
+package patterns;
+
+public class NumberPattern2 {
+
+	public static void main(String[] args) {
+
+		
+		
+	}
+
+}
---
+++ Basics/src/patterns/Patter1.java
@@ -0,0 +1,18 @@
+package patterns;
+
+public class Patter1 {
+
+	public static void main(String[] args) {
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
